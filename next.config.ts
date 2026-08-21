966f7f77af99be62df13814f99790e8f156562dd
Alex Puertas <alexpuertasr@gmail.com>
2026-08-21 11:51:52 +1000

    feat: allow remote images from Google user avatars

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
